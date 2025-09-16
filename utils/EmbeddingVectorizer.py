994642147b15de98358fac354388c6f358ebd9ab
Vu Tuan Anh <vutuananh23@gmail.com>
2025-09-16 22:03:55 +0700

    feat: update EmbeddingVectorizer.py

--- utils/EmbeddingVectorizer.py
+++ utils/EmbeddingVectorizer.py
@@ -45,11 +45,41 @@ class EmbeddingVectorizer(object):
         return np.array(self.transform(texts, mode=mode))
 
 
-def tfidf_vectorizer(texts: List[str]):
-    pass
+def tfidf_vectorizer(X_train, X_test) -> numpy.ndarray:
+    """
+    function to return the numpy array of train & test transformed from TfidfVectorizer
+    :param X_train: train features
+    :param X_test: test features
+    :return: numpy array of train & test transformed from TfidfVectorizer
+    """
+    from sklearn.feature_extraction.text import TfidfVectorizer
+    tfidf = TfidfVectorizer(tokenizer=lambda text: parse_text(text))
+    X_train_tfidf = tfidf.fit_transform(X_train)
+    X_test_tfidf = tfidf.transform(X_test)
+    return np.array(X_train_tfidf.toarray()), np.array(X_test_tfidf.toarray())
 
-def bow_vectorizer(texts: List[str]):
-    pass
+def bow_vectorizer(X_train, X_test) -> numpy.ndarray:
+    """
+    function to return the numpy array of train & test transformed from CountVectorizer
+    :param X_train: train features
+    :param X_test: test features
+    :return: numpy array of train & test transformed from CountVectorizer
+    """
+    from sklearn.feature_extraction.text import CountVectorizer
+    ctv = CountVectorizer(tokenizer=lambda text: parse_text(text))
+    X_train_bow = ctv.fit_transform(X_train)
+    X_test_bow = ctv.transform(X_test)
+    return np.array(X_train_bow.toarray()), np.array(X_test_bow.toarray())
 
-def embedding_vectorizer(texts: List[str]):
-    pass
+def embedding_vectorizer(X_train, X_test) -> numpy.ndarray:
+    """
+    function to return the numpy array of train & test transformed from EmbeddingVectorizer
+    :param X_train: train features
+    :param X_test: test features
+    :return: numpy array of train & test transformed from EmbeddingVectorizer
+    """
+    vectorizer = EmbeddingVectorizer()
+    X_train_vt = vectorizer.transform_numpy(X_train)
+    X_test_vt = vectorizer.transform_numpy(X_test)
+    return X_train_vt, X_test_vt
+
